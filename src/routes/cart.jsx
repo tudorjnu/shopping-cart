4d349851e1e681c208d19fe521d105cfaeefb14b
Tudor Jianu <jianu.tudor9@gmail.com>
2024-02-15 07:56:23 +0000

    add state management on fetch

--- src/routes/cart.jsx
+++ src/routes/cart.jsx
@@ -1,5 +1,5 @@
 import styles from "./home.module.scss";
-import { getProducts } from "../lib/actions";
+import { useEffect, useState } from "react";
 
 function Item({ title, price }) {
   console.log(title);
@@ -12,9 +12,31 @@ function Item({ title, price }) {
   );
 }
 
+function useProducts() {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products?limit=5")
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("server error");
+        }
+        return response.json();
+      })
+      .then((response) => setProducts(response))
+      .catch((error) => setError(error))
+      .finally(setLoading(false));
+  }, []);
+
+  return { products, error, loading };
+}
 function Cart() {
-  const products = getProducts();
-  console.log(products[0]);
+  const { products, error, loading } = useProducts();
+
+  if (error) return <p>an error has been encountered</p>;
+  if (loading) return <p>Loading...</p>;
 
   return (
     <>
